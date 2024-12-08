b1484299c34cce457a7caa5080743d70440c0e28
Joelle Saliba <joellesaliba82@gmail.com>
2024-12-08 18:17:14 +0200

    final project

---
+++ script.js
@@ -0,0 +1,34 @@
+//selector 
+
+const output = document.querySelector(".output");
+const result = document.querySelector(".result");
+const keys = document.querySelectorAll("button");
+
+//eventlistener
+keys.forEach(key=>{
+    key.addEventListener("click",calculate);
+});
+
+function calculate(){
+    let buttonText = this.innerText;
+    switch(buttonText) {
+        case "AC":
+            output.innerText = "";
+            result.innerText = "0";
+            result.style.animation = "";
+            output.style.animation = "";
+            break;
+        case "DEL":
+            output.textContent = output.textContent.substr(0,output.textContent.length-1);
+            break;
+        case "=":
+            result.innerText = eval(output.innerText);
+            result.style.animation = "big 0.5s ease-in-out";
+            output.style.animation = "small 0.5s ease-in-out";
+            result.style.animationFillMode = "forwards";
+            output.style.animationFillMode = "forwards";
+            break;
+        default:
+            output.textContent += buttonText;
+            break;
+    }}
