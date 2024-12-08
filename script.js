a8ddc0fc9ae1bd7b07610f3319e6fb59ea03bd0c
Joelle Saliba <joellesaliba82@gmail.com>
2024-12-08 19:01:14 +0200

    added keyboard listeners

--- script.js
+++ script.js
@@ -32,3 +32,31 @@ function calculate(){
             output.textContent += buttonText;
             break;
     }}
+
+window.addEventListener('keydown', (event) => {
+    key = event.key;
+    handlePress(key);
+})
+
+function handlePress(key) {
+    if (key == "a") {
+        output.innerText = "";
+        result.innerText = "0";
+        result.style.animation = "";
+        output.style.animation = "";
+        return;
+    } else if (key == "=" || key == "Enter") {
+        result.innerText = eval(output.innerText);
+        result.style.animation = "big 0.5s ease-in-out";
+        output.style.animation = "small 0.5s ease-in-out";
+        result.style.animationFillMode = "forwards";
+        output.style.animationFillMode = "forwards";
+        return;
+    } else if (key >= "0" && key <= "9" || key == "+" || key == "-" || key == "*" || key == "/" || key == "(" || key == ")" || key == ".") {
+        output.textContent += key;
+        return;
+    } else if (key == "Backspace") {
+        output.textContent = output.textContent.substr(0,output.textContent.length-1);
+        return;
+    }
+}
